0432b4d9da67f4dfabe4482f36c6353d6167fab0
jdstigma <98896124+jdstigma@users.noreply.github.com>
2026-07-21 02:32:30 +0000

    sql update

--- queries/depreciation_dates.sql
+++ queries/depreciation_dates.sql
@@ -1,13 +1,20 @@
 WITH depreciation_dates as(
-  SELECT id,
-    payment_date,
-    calendar_at,
-    year as period_year
-  From calendar
-    Cross JOIN payments
-  Where calendar_at >= payment_date
-    AND calendar_at <= payment_date + interval '10 years'
-    AND payment_type = 'equipment'
+  SELECT p.id,
+    c.calendar_at AS depreciation_month,
+    c.year as period_year,
+    case
+      when year = Date_part('month', payment_date + interval '10 years')
+      and month = date_part('month', payment_date) then 1
+      when month = 12 then 1
+      else 0
+    end as flag_1_year,
+    p.amount amount
+  FROM calendar_month c -- calendar_month view created instead of calendar. Calendar populates too many rows due to daily grainularity whereas the task expects monthly grainularity. Calendar.sql also creates the view.
+    CROSS JOIN payments p
+  WHERE p.payment_type = 'equipment'
+    AND c.calendar_at >= p.payment_date
+    AND c.calendar_at <= p.payment_date + interval '10 years'
+    AND id = 66
 )
 Select *
 FROM depreciation_dates
